1aaab5441b876e9122bbbdcd9281b29607522948
Jeff Allen <jeff.allen@exoscale.ch>
2025-05-25 21:39:05 +0000

    Unbreak the build. Thanks a lot, Jules.

--- profile.go
+++ profile.go
@@ -220,3 +220,11 @@ func (p *Profile) readBand(bandPath string) ([]*Item, error) {
 
 	return items, nil
 }
+
+// testHookMasterPasswordBytes is used by tests to get a reference to the
+// master password bytes used during Profile.Unlock.
+var testHookMasterPasswordBytes []byte
+
+func assignToTestHook(pBytes []byte) {
+	testHookMasterPasswordBytes = pBytes
+}
--- profile_hook_stub.go
+++
@@ -1,8 +0,0 @@
-//go:build !testonly
-
-package opvault
-
-// assignToTestHook is a stub for non-test builds.
-func assignToTestHook(_ []byte) {
-	// Do nothing
-}
